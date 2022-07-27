2521d8b8bb7bca9af65a91f1b52b571cf93f8519
lxexjx <eun.leee1377@gmail.com>
2022-07-27 12:42:35 +0900

    commit 공통 인터페이스 생성

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,9 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@EnableJpaRepositories(basePackages ="study.datajpa.repositories")
 @SpringBootApplication
 public class DataJpaApplication {
 
