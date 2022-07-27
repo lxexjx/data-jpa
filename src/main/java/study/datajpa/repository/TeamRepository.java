4fe00437499dc5551ba293bb112f8d56a4ba43a8
lxexjx <eun.leee1377@gmail.com>
2022-07-27 12:45:18 +0900

    commit 공통 인터페이스 생성

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -3,5 +3,5 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
-public interface TeamRepository extends JpaRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, Long> {//<엔티티, 엔티티(PK)에 매핑된 타입>
 }
