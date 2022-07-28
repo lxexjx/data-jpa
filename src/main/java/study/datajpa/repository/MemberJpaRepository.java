499ed29b5dce01320607f219aa12a856d07206c2
lxexjx <eun.leee1377@gmail.com>
2022-07-28 12:37:22 +0900

    commit JPA페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -43,7 +43,15 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
-    public List<Member> findByUsername(String username) {
-        List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class).setParameter("username", username).getResultList();
+//    public List<Member> findByUsername(String username) {
+//        List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class).setParameter("username", username).getResultList();
+//    }
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                        .setParameter("age", age).setFirstResult(offset).setMaxResults(limit).getResultList();
+    }
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class).setParameter("age", age).getSingleResult();
     }
 }
