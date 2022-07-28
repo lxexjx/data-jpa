a4980ffd36c7881c5b92fc9bf59435da5cca1183
lxexjx <eun.leee1377@gmail.com>
2022-07-28 12:30:00 +0900

    commit 파라미터 바인딩, 반환타입

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -26,5 +26,12 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " + "from Member m join m.team t")
     List<MemberDto> findMemberDto();
 
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String name); //컬렉션
+    Member findMemberByUsername(String name); //단건
+
+
 }
 
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -10,7 +10,9 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -127,5 +129,33 @@ class MemberRepositoryTest {
         for(MemberDto dto : memberDto){
             System.out.println("dto = "dto);
         }
+    }
+
+    @Test
+    public void findByNames(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result =  memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+        for(Member member: result){
+            System.out.println("member = "+ member);
+        }
+    }
+
+    @Test
+    public void returnType(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //List<Member> aaa = memberRepository.findListByUsername("AAA");
+        //Member findMember = memberRepository.findMemberByUsername("AAA");
+        //Optional<Member> aaa= memberRepository.findOptionalByUsername("AAA");
+        List<Member> result = memberRepository.findListByUsername("asdf");
     }
 }
