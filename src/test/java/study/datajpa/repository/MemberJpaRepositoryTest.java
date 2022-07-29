0cefdf9d3f167593b34a094887230c57f0b3a33a
lxexjx <eun.leee1377@gmail.com>
2022-07-29 16:33:35 +0900

    commit 벌크성 수정 쿼리

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -105,4 +105,18 @@ class MemberJpaRepositoryTest {
         Assertions.assertThat(members.size()).isEqualTo(3);
         Assertions.assertThat(totalCount).isEqualTo(5);
     }
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        //given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 19));
+        memberJpaRepository.save(new Member("member3", 20));
+        memberJpaRepository.save(new Member("member4", 21));
+        memberJpaRepository.save(new Member("member5", 40));
+        //when
+        int resultCount = memberJpaRepository.bulkAgePlus(20);
+        //then
+        Assertions.assertThat(resultCount).isEqualTo(3);
+    }
 }
