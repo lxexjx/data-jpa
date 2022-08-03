bcabe1a59265b4e5251268e88f8cbf30c1f3a5ea
lxexjx <eun.leee1377@gmail.com>
2022-08-03 16:45:39 +0900

    commit 도메인 클래스 컨버터

--- src/main/java/study/datajpa/controller/HelloController.java
+++ src/main/java/study/datajpa/controller/HelloController.java
@@ -1,9 +1,11 @@
 package study.datajpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "")
 public class HelloController {
 
     @GetMapping("/hello")
